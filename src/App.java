5dda85fdfc77c77a15cf70f14cbbd9234e09f6ed
Zarpperia <castiellgames@gmail.com>
2024-04-15 20:36:36 -0300

    Produto

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HashSetExemplo1();
+    }
+
+    public static void HashSetExemplo1() {
+        HashSet<String> cars = new HashSet<String>();
+        System.out.println(cars.add("Volvo"));
+        System.out.println(cars.add("BMW"));
+        System.out.println(cars.add("Ford"));
+        System.out.println(cars.add("BMW"));
+        System.out.println(cars.add("Mazda"));
+        System.out.println(cars);
+    }
+    public static void HashSetExemplo2() {
+        
+    }
+}
