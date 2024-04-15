9811011b2d2b93b1136f13957a635c51faca96b3
Zarpperia <castiellgames@gmail.com>
2024-04-15 20:36:43 -0300

    List

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HashSetExemplo1();
+        HashSetExemplo2();
     }
 
     public static void HashSetExemplo1() {
@@ -14,7 +14,19 @@ public class App {
         System.out.println(cars.add("Mazda"));
         System.out.println(cars);
     }
+
     public static void HashSetExemplo2() {
+        HashSet<Produto> catalogo = new HashSet<Produto>();
         
-    }
+        //Criei um objeto do tipo Produto com dados do Nintendo Switch
+        Produto oProdSwitch = new Produto("Nintendo", "Switch", 1899);
+        System.out.println(catalogo.add(oProdSwitch));
+
+        //Criei um objeto do tipo Produto com dados do Master
+        Produto oProdMaster = new Produto("Tectoy", "Master System", 360.05f);
+        System.out.println(catalogo.add(oProdMaster));
+
+        //Tentando colocar o objeto do Switch DE NOVO!
+        System.out.println(catalogo.add(oProdSwitch));
+    }  
 }
